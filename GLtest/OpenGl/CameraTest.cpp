91a6911f8ae994839b00dafb0e3026cb42c6406b
ZhengMingQi <867673046@qq.com>
2019-10-01 15:29:54 +0800

    depend

---
+++ GLtest/OpenGl/CameraTest.cpp
@@ -0,0 +1,169 @@
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+#include "stb_image.h"
+#include"Shader.h"
+#include<iostream>
+#include<glm/glm.hpp>
+#include<glm/gtc//type_ptr.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void initVAO();
+
+const unsigned int SCR_W = 800;
+const unsigned int SCR_H = 600;
+
+
+int main() {
+	// ===============================  init ============================================ 
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	GLFWwindow* window = glfwCreateWindow(SCR_W, SCR_H, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// glad: load all OpenGL function pointers
+	// ---------------------------------------
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+	// =================================================================
+	Shader mShader("Shaders/TextureTest.vs", "Shaders/TextureTest.fs");
+	initVAO();
+
+	//Frame  Loop
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		mShader.use();
+		// 手动设置纹理单元
+		mShader.setInt("texture1", 0);
+		mShader.setInt("texture2", 1);
+
+		// 变换矩阵
+		glm::mat4 transform = glm::mat4(1.0f);//, rotateMatrix,translateMatrix, scaleMatrix;
+		transform = glm::rotate(transform, glm::radians(-55.0f), glm::vec3(1.0f, 0.5f, 0.0f));
+		 
+		//transform = rotateMatrix;// * translateMatrix * scaleMatrix;
+		//transform = glm::translate(transform, glm::vec3(0.0f,0.0f,-3.0f));
+		glUniformMatrix4fv(glGetUniformLocation(mShader.ID, "transform"), 1, GL_FALSE, glm::value_ptr(transform));
+
+		//glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		//glLineWidth(1);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+		// -------------------------------------------------------------------------------
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+
+}
+
+// resize window
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
+}
+
+void initVAO() {
+	/* float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	 0.5f,  0.5f, 0.0f,
+	 -0.5f, 0.5f, 0.0f
+	};*/
+
+	float vertices[] = {
+		// positions          // colors           // texture coords
+		 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left 
+	};
+
+	unsigned int index[] = {
+		0,1,2,
+		2,3,0
+	};
+
+	unsigned int mVAO, mVBO, mEBO;
+	glGenBuffers(1, &mEBO);
+	glGenVertexArrays(1, &mVAO);
+	glGenBuffers(1, &mVBO);
+
+	glBindVertexArray(mVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, mVBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mEBO);
+
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index), index, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// pos Attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
+	glEnableVertexAttribArray(0);
+	// color Attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	// texure coord attribute
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+
+	//texture1
+	unsigned int mTexture;
+	glGenTextures(1, &mTexture);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, mTexture);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+
+	int mWidth, mHeight, mChannel;
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char * data = stbi_load("container.jpg", &mWidth, &mHeight, &mChannel, 0);
+	if (!data) {
+		std::cout << "pic not found";
+	}
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, mWidth, mHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	//texture2
+	unsigned int mTexture2;
+	glGenTextures(1, &mTexture2);
+	glActiveTexture(GL_TEXTURE1);
+	glBindTexture(GL_TEXTURE_2D, mTexture2);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	data = stbi_load("awesomeface.png", &mWidth, &mHeight, &mChannel, 0);
+	if (!data) {
+		std::cout << "pic not found";
+	}
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, mWidth, mHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	// push uniform
+
+}
